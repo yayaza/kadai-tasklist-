a82739d9025836277d78f8bcbbc43b37f4fab44f
Toshihide Yazawa <jonsute0420@gmail.com>
2017-04-26 02:09:10 +0000

    bootstrap design

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
  root to: 'tasks#index'
   resources :tasks
 end
